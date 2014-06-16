bb6fe85dfbc57c28395e76d3c8a3bdd992990606
oth-YX <oth-YX@users.noreply.github.com>
2014-06-16 16:38:56 +0300

    -

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('I am a NodeJS test server running in a Linux container.\n');
-  res.send('\nMIT Licence 2014 - aoth');
+  res.send('I am a NodeJS test server running in a Linux container.\n -aothman-\n');
 });
 
 app.listen(PORT);
